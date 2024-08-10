916645e32cf21164663b2c5454e422a280be1ff9
BenedictPresley <19623252@std.stei.itb.ac.id>
2024-08-10 23:29:41 +0700

    Added more data to about section

--- portfolio/src/app/components/AboutSection.jsx
+++ portfolio/src/app/components/AboutSection.jsx
@@ -14,15 +14,75 @@ const itemData = [
     {
         id: 2,
         title: "Institut Teknologi Bandung",
-        description: "2023-2027",
+        description: "2023 - Present",
         tag: ["education"],
     },
     {
         id: 3,
+        title: "Python",
+        description: "Intermediate level. Able to use many external Python libraries such as pandas, numpy, cryptodome, etc.",
+        tag: ["skills"],
+    },
+    {
+        id: 4,
+        title: "C++",
+        description: "Able to use procedural and OOP concepts to compete in high level competitions",
+        tag: ["skills"],
+    },
+    {
+        id: 5,
+        title: "Haskell",
+        description: "Understand the concepts of Functional Programming",
+        tag: ["skills"],
+    },
+    {
+        id: 6,
+        title: "ROS",
+        description: "Able to build software for autonomous drones",
+        tag: ["skills"],
+    },
+    {
+        id: 7,
+        title: "HTML/CSS/JS",
+        description: "Intermediate proficiency",
+        tag: ["skills"],
+    },
+    {
+        id: 8,
         title: "OSN Informatika 2022 Gold Medalist",
         description: "Achieved Gold medal at Indonesia's most prestigious competition for high school students",
         tag: ["achievements"],
     },
+    {
+        id: 9,
+        title: "2024 ICPC Asia Pacific Championship Finalist",
+        description: "Participated in the largest competitive programming competition in Asia region",
+        tag: ["achievements"],
+    },
+    {
+        id: 10,
+        title: "2023 ICPC Asia-Jakarta Regional Finalist",
+        description: "Participated in the largest competitive programming competition in Indonesia region",
+        tag: ["achievements"],
+    },
+    {
+        id: 11,
+        title: "2nd Runner Up CompFest 15 UI",
+        description: "5th place in CompFest 15 senior competitive programming category",
+        tag: ["achievements"],
+    },
+    {
+        id: 12,
+        title: "VTOL Drone",
+        description: "Developed software for autonomous drones with vertical takeoff and landing capabilities",
+        tag: ["projects"]
+    },
+    {
+        id: 13,
+        title: "STEI-K ITB'23 Election System",
+        description: "Created a software to help ensure fair voting in STEI-K ITB'23 leader election",
+        tag: ["projects"]
+    }
 ];
 
 const AboutSection = () => {
